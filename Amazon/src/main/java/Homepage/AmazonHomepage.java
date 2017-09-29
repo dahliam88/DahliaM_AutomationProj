6ee6efdcd956406302c45e30601896b60988cd8e
DahliaM <dahliam@live.com>
2017-09-29 14:16:49 -0400

    ExtentReportWorking

--- Amazon/src/main/java/Homepage/AmazonHomepage.java
+++ Amazon/src/main/java/Homepage/AmazonHomepage.java
@@ -10,45 +10,17 @@ import static java.lang.Thread.sleep;
 
 public class AmazonHomepage extends CommonAPI {
 
-   @FindBy(how = How.XPATH, using = ".//*[@id='gw-desktop-herotator']/div/div/div/div[3]/a/i")
-    public static WebElement photoGallery;
-
-    @FindBy(how = How.XPATH, using = ".//*[@id='gw-desktop-herotator']/div/div/div/div[3]/a/i")
-    public static WebElement photoGallery2;
-
-    @FindBy(how = How.XPATH, using = ".//*[@id='gw-desktop-herotator']/div/div/div/div[3]/a/i")
-    public static WebElement photoGallery3;
-
-    @FindBy(how = How.XPATH, using = ".//*[@id='gw-desktop-herotator']/div/div/div/div[3]/a/i")
-    public static WebElement photoGallery4;
-
     @FindBy(how = How.XPATH, using = ".//*[@id='twotabsearchtextbox']")
     public static WebElement searchItems;
 
     @FindBy(how = How.XPATH, using = ".//*[@id='nav-search']/form/div[2]/div/input")
     public static WebElement searchBtn;
 
-
     public void searchInput(String item) throws InterruptedException {
-        System.out.println("Scroll through photo gallery 4 times");
-
-        photoGallery.click();
-        sleep(3000);
-
-        photoGallery2.click();
-        sleep(3000);
-
-        photoGallery3.click();
-        sleep(3000);
-
-        photoGallery4.click();
-        sleep(3000);
-
         searchItems.sendKeys(item);
         sleep(3000);
 
         searchBtn.click();
         sleep(3000);
     }
-
 }
--- Generic/src/main/java/common/CommonAPI.java
+++ Generic/src/main/java/common/CommonAPI.java
@@ -1,5 +1,9 @@
 package common;
 
+import Reporting.ExtentManager;
+import Reporting.ExtentTestManager;
+import com.aventstack.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.*;
@@ -12,20 +16,87 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CommonAPI {
 
+    public static com.relevantcodes.extentreports.ExtentReports extent;
+
+    @BeforeSuite
+    public void extentSetup(ITestContext context) {
+        ExtentManager.setOutputDirectory(context);
+        extent = ExtentManager.getInstance();
+    }
+
+    @BeforeMethod
+    public void startExtent(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        String methodName = method.getName().toLowerCase();
+        ExtentTestManager.startTest(method.getName());
+        ExtentTestManager.getTest().assignCategory(className);
+    }
+
+    protected String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+    @AfterMethod
+    public void afterEachTestMethod(ITestResult result) {
+        ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getEndMillis()));
+
+        for (String group : result.getMethod().getGroups()) {
+            ExtentTestManager.getTest().assignCategory(group);
+        }
+
+        if (result.getStatus() == 1) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "Test Passed");
+        } else if (result.getStatus() == 2) {
+            ExtentTestManager.getTest().log(LogStatus.FAIL, getStackTrace(result.getThrowable()));
+        } else if (result.getStatus() == 3) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+        }
+
+        ExtentTestManager.endTest();
+
+        extent.flush();
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            captureScreenshot(driver, result.getName());
+        }
+        driver.quit();
+    }
+
+    @AfterSuite
+    public void generateReport() {
+        extent.close();
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+
+//-----------------------------------//
 public static WebDriver driver=null;
 private String saucelabs_username="dahliam324";
 private String browserstack_username="dahliamarcano1";
@@ -69,7 +140,6 @@ public WebDriver getLocalDriver(@Optional("mac") String OS,String browserName){
         driver=new FirefoxDriver();
         }
         return driver;
-
         }
 public WebDriver getLocalGridDriver(String browserName){
         if(browserName.equalsIgnoreCase("chrome")){
@@ -147,7 +217,6 @@ public List<String> getTextFromWebElements(String locator){
         for(WebElement web:element){
         text.add(web.getText());
         }
-
         return text;
         }
 public List<WebElement> getListOfWebElementsByCss(String locator){
